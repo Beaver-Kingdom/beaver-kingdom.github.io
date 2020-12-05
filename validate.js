4b32b1b1549680265d7a97cfe1424cc628963e75
Beaver Kingdom <75463454+iamthegreatbeaver@users.noreply.github.com>
2020-12-04 23:14:11 -0700

    Add files via upload
    Java Doc

---
+++ validate.js
@@ -0,0 +1,5 @@
+  function showMenuBar() {
+      document.getElementById('Hello').innerHTML="Hello. Please click an item on the menu to begin!"
+      document.getElementById('enterBtn').style.visibility="hidden"
+      document.getElementById('menuBar').style.visibility="visible"
+  }
